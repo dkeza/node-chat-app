528cf32f62d9b6f3cd7589d6fea5bf166de50364
keza <dk@citadel.rs>
2018-08-25 13:44:15 +0200

    Show when user is connected or disconnected

--- server/server.js
+++ server/server.js
@@ -7,8 +7,17 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 server.listen(port, () => {
     console.log(`Started on port ${port}`);
