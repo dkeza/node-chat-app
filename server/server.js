2a1950862c167fd8b7c364efbf647e1f51c2c8b0
keza <dk@citadel.rs>
2018-08-29 18:46:09 +0200

    Acknowledgements

--- server/server.js
+++ server/server.js
@@ -19,10 +19,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage("Admin", "New user joined"));
 
-    socket.on('createMessage', (data) => {
+    socket.on('createMessage', (data, callback) => {
         console.log('createMessage', data);
         io.emit('newMessage', generateMessage(data.from, data.text));
-
+        callback('This is from the server.');
         // socket.broadcast.emit('newMessage', {
         //         from: data.from,
         //         text: data.text,
